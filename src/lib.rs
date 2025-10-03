66340b307b104632a806528768b951e1c8277a33
espand-masoon <espandmasoon@gmail.com>
2025-10-03 19:00:00 +0330

    Add the feature to toggle case-sensivity

--- src/lib.rs
+++ src/lib.rs
@@ -1,9 +1,19 @@
-pub fn search<'a>(query: &str, content: &'a str) -> Vec<&'a str> {
+pub fn search<'a>(query: &str, content: &'a str, case_sensitive: bool) -> Vec<&'a str> {
     let mut result_lines: Vec<&str> = Vec::new();
 
-    for line in content.lines() {
-        if line.contains(query) {
-            result_lines.push(line);
+
+    if case_sensitive {
+        for line in content.lines() {
+            if line.contains(query) {
+                result_lines.push(line);
+            }
+        }
+    } else {
+        let query = query.to_lowercase();
+        for line in content.lines() {
+            if line.to_lowercase().contains(&query) {
+                result_lines.push(line);
+            }
         }
     }
 
@@ -15,11 +25,22 @@ mod test {
     use super::*;
 
     #[test]
-    fn it_works() {
+    fn case_sensitive() {
         let content = "\
 Rust:
 Safe, Fast, Productive.
-Pick three!";
-        assert_eq!(search("ast", content), vec!["Safe, Fast, Productive."]);
+Pick three!
+there is no PAST";
+        assert_eq!(search("ast", content, true), vec!["Safe, Fast, Productive."]);
+    }
+
+    #[test]
+    fn case_insensitive() {
+        let content = "\
+Rust:
+Safe, Fast, Productive.
+Pick three!
+There is no PAST";
+        assert_eq!(search("ast", content, false), vec!["Safe, Fast, Productive.", "There is no PAST"]);
     }
 }
--- src/main.rs
+++ src/main.rs
@@ -20,7 +20,7 @@ fn main() {
 fn run(config: Config) -> Result<(), io::Error> {
     // Reading the file content
     let file_content = fs::read_to_string(config.file_path)?;
-    let result_lines = search(config.query, &file_content);
+    let result_lines = search(config.query, &file_content, config.case_sensitive);
     for line in result_lines {
         println!("{line}");
     }
@@ -30,12 +30,31 @@ fn run(config: Config) -> Result<(), io::Error> {
 struct Config<'a> {
     query: &'a str,
     file_path: &'a str,
+    case_sensitive: bool,
 }
 
 impl<'a> Config<'a> {
     fn build(args: &'a Vec<String>) -> Result<Self, &'static str> {
         if args.len() < 3 {
-            return Err("The command needs 2 arguments!");
+            return Err("The command needs at least 2 arguments!");
+        }
+
+        // Check environment variables
+        let mut case_sensitive: bool = true;
+        if let Ok(case_sensitive_env) = env::var("CASE_SENSITIVE") {
+            match case_sensitive_env.to_lowercase().as_str() {
+                "false" | "0" => case_sensitive = false,
+                _ => {},
+            }
+        }
+
+        // Check command options
+        let mut args_iter = args.iter().skip(3);
+        while let Some(argument) = args_iter.next() {
+            match argument.as_str() {
+                "--case-insensitive" | "-ci" => {case_sensitive = false},
+                _ => {},
+            }
         }
 
         let query = &args[1];
@@ -43,6 +62,7 @@ impl<'a> Config<'a> {
         Ok(Self {
             query,
             file_path,
+            case_sensitive,
         })
     }
 }
